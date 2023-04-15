cf743d92debe1e0fa51ae767334003b406447e77
raii <raii.program@gmail.com>
2023-04-15 19:27:49 +0900

    mogral-cliにエラー表示を追加

--- mogral-cli/src/main.rs
+++ mogral-cli/src/main.rs
@@ -4,6 +4,8 @@ use std::io::{self, Read};
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
-    let ast = mogral::parse(&buffer).unwrap();
-    println!("{:?}", ast);
+    match mogral::parse(&buffer) {
+        Ok(ast) => println!("{:?}", ast),
+        Err(err) => eprintln!("{}", err),
+    }
 }
